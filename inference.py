eb80ddd0c78c89511ba1406c84db0c55c0480ac3
sakibchowdhury131 <sakibchowdhury131@gmail.com>
2023-03-23 14:12:37 +0600

    inference scheme updated

--- inference.py
+++ inference.py
@@ -1,6 +1,9 @@
 from StockCore import StockPredictor
 from StockCore import StockDataset
 from torch.utils.data import DataLoader
+from tqdm import tqdm
+from get_data import get_data
+import torch
 
 def inference(x,
               checkpoint = 'checkpoints/lin-conv-lin-out1/epoch=294-step=1770.ckpt',
@@ -13,17 +16,24 @@ def inference(x,
         model = StockPredictor.load_from_checkpoint(checkpoint_path=checkpoint, learning_rate=learning_rate, batch_size=batch_size).eval().cuda(device=0)
     elif device == 'cpu':
         model = StockPredictor.load_from_checkpoint(checkpoint_path=checkpoint, learning_rate=learning_rate, batch_size=batch_size).eval()
-    pred = model(x)
+    pred = model(x).detach().numpy()
     return pred
 
 if __name__ == '__main__':
     
-    stockDataset = StockDataset('database.csv',  inp_len = 128, out_len = 2)
-    dataloader = DataLoader(stockDataset, batch_size = 1, shuffle = True, num_workers = 2)
-    dataiter = iter(dataloader)
-    input, output = next(dataiter)
-    pred = inference(input).detach().numpy()
-
-    print(output.numpy(), pred)
+    organization_name = 'OLYMPIC'
+    database = {}
+    with open('database.csv', 'r') as f:
+      lines = f.readlines()
+    
+    for line in tqdm(lines):
+        database[line.split(',')[0]] = line.split(',')[1]
+    
+    URL = database[organization_name]
+    data = get_data(URL)
+    input = torch.FloatTensor(data[-129:-1]).unsqueeze(0)
+    pred = inference(input)
+    print('last day price (BDT):',input[0, -1].numpy())
+    print('Next 2 days Predicted LTP (BDT):',pred)
 
 
